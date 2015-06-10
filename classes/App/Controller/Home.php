38a3bd17a93f32c2ca52d7d0470dca4fe348317f
Legros Corentin <corentin.legros@gmail.com>
2015-06-10 19:01:39 +0200

    Ajout contrôleur principale + vue

--- classes/App/Controller/Home.php
+++ classes/App/Controller/Home.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Page;
+use App\Tools\SEStrategy\SearchHandler;
 
 class Home extends Page {
 
 	public function action_index() {
-
+        SearchHandler::search('lolilalul');
 	}
 }
